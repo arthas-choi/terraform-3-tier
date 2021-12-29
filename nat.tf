b992949596c1e7eccadcda29be68a50a688760f9
김지선Jiseon Kim <jiseon.kim@bespinglobal.com>
2021-12-29 18:30:03 +0900

    nat gateway 설정 변경

--- nat.tf
+++ nat.tf
@@ -7,7 +7,7 @@ resource "aws_eip" "tier_eip" {
 
 resource "aws_nat_gateway" "tier-nat" {
   allocation_id = aws_eip.tier_eip.id
-  subnet_id = aws_subnet.tier-public-subnet-mgmt.id
+  subnet_id = aws_subnet.tier-public-subnet-1.id
   tags = {
     Name = "${var.resource_prefix}-Nat-Gateway"
   }
