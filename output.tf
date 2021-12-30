f251829201683b646c5b135a961de9d23c4959fb
김지선Jiseon Kim <jiseon.kim@bespinglobal.com>
2021-12-30 18:40:59 +0900

    output.tf 수정

--- output.tf
+++ output.tf
@@ -1,20 +1,10 @@
-output "subnet-web-ids" {
-  value = [aws_subnet.tier-public-subnet-1.id, aws_subnet.tier-public-subnet-2.id]
-  description = "Public Subnet ID List"
-}
-
-output "web-sg-id" {
-  value = aws_security_group.web-lc-sg.id
-  description = "Bastion Security Group id"
-}
-
 output "bastion-public-ip" {
   value = aws_instance.bastion-ec2.public_ip
   description = "Bastion Public IP Address"
 }
 
-output "was-alb-dns" {
-  value = aws_lb.was-alb.dns_name
+output "web-alb-dns" {
+  value = aws_lb.web-alb.dns_name
   description = "WAS ALB DNS Name"
 }
 
