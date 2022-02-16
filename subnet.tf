25296117556569485d77bdc032c402ff520e9632
Arthas Choi <24883918+arthas-choi@users.noreply.github.com>
2022-02-16 21:05:43 +0900

    Update subnet.tf

--- subnet.tf
+++ subnet.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "tier-public-subnet-1" {
 resource "aws_subnet" "tier-public-subnet-2" {
   vpc_id = aws_vpc.tier-vpc.id
   cidr_block = cidrsubnet(var.subnet_cidr, 4, 2)
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = "${length(data.aws_availability_zones.available.names)>3 ? data.aws_availability_zones.available.names[2] : data.aws_availability_zones.available.names[1]}"
   map_public_ip_on_launch=true
   tags = {
     Name = "${var.resource_prefix}-PUB-2"
@@ -30,7 +30,7 @@ resource "aws_subnet" "tier-web-private-subnet-1" {
 resource "aws_subnet" "tier-web-private-subnet-2" {
   vpc_id = aws_vpc.tier-vpc.id
   cidr_block = cidrsubnet(var.subnet_cidr, 4, 4)
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = "${length(data.aws_availability_zones.available.names)>3 ? data.aws_availability_zones.available.names[2] : data.aws_availability_zones.available.names[1]}"
   tags = {
     Name = "${var.resource_prefix}-WEB-PRV-2"
   }
@@ -48,7 +48,7 @@ resource "aws_subnet" "tier-was-private-subnet-1" {
 resource "aws_subnet" "tier-was-private-subnet-2" {
   vpc_id = aws_vpc.tier-vpc.id
   cidr_block = cidrsubnet(var.subnet_cidr, 4, 6)
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = "${length(data.aws_availability_zones.available.names)>3 ? data.aws_availability_zones.available.names[2] : data.aws_availability_zones.available.names[1]}"
   tags = {
     Name = "${var.resource_prefix}-WAS-PRV-2"
   }
@@ -66,7 +66,7 @@ resource "aws_subnet" "tier-db-subnet-1" {
 resource "aws_subnet" "tier-db-subnet-2" {
   vpc_id = aws_vpc.tier-vpc.id
   cidr_block = cidrsubnet(var.subnet_cidr, 4, 8)
-  availability_zone = data.aws_availability_zones.available.names[1]
+  availability_zone = "${length(data.aws_availability_zones.available.names)>3 ? data.aws_availability_zones.available.names[2] : data.aws_availability_zones.available.names[1]}"
   tags = {
     Name = "${var.resource_prefix}-DB-2"
   }
